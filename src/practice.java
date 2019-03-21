3c4b7e79009125013073bde433306186f6841bf4
Anwar Lopez <anwarlo247@gmail.com>
2019-03-21 11:19:23 -0500

    added file for in-class follow along

---
+++ src/practice.java
@@ -0,0 +1,28 @@
+public class practive {
+    public static void main(String[] args) {
+//        System.out.println("start");
+//        for (int i = 0; //(int i = 0; i<10;i++)
+//        i < 10;
+//        i++ ) {
+//            System.out.println(i);
+//        }
+//        System.out.println("done");
+//        //i = 0,1,2,3,4,5,6,7,8,9
+
+//        for (int i = 1; i <= 10; i++){
+//            if (i % 2 != 0){
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+        for (int i = 1; true; i++){
+            if (i > 10) break;
+            if (i % 2 != 0) {
+                continue;
+            }
+            System.out.println(i);
+
+    }
+
+}
+}
