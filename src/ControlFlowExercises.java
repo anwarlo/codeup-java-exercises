e43f63d572835ba9ff9bbd1d8fad9723145b923c
Anwar Lopez <anwarlo247@gmail.com>
2019-03-21 11:39:39 -0500

    completed exercise 1b do while loop count by 2s to 100

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
+        int i = 0;
+        do {//statements
             System.out.println(i);
-            i++;
-        }
+            i += 2;
+        } while (i <= 100);//condition goes in parenthesis.
+
     }
 }
+//    Do While
+//
+//    Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//        Alter your loop to count backwards by 5's from 100 to -10.
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
