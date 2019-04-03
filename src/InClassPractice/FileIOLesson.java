993d241f5f259055260e4ab6144b15cd0cdcafc9
Anwar Lopez <anwarlo247@gmail.com>
2019-04-03 12:23:54 -0500

    in class lesson 2

--- src/InClassPractice/FileIOLesson.java
+++ src/InClassPractice/FileIOLesson.java
@@ -4,41 +4,50 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.lang.String;
 
 
 public class FileIOLesson {
 
     public static void main(String[] args) {
 
-    String directory = "data";
-    String filename = "IOtest.txt";
-
-    Path dataDirectory = Paths.get(directory);
-    Path dataFile = Paths.get(directory,filename);
+//    String directory = "data";
+//    String filename = "IOtest.txt";
+//
+//    Path dataDirectory = Paths.get(directory);
+//    Path dataFile = Paths.get(directory,filename);
 
     try{
-        if (Files.notExists(dataDirectory)){
-            Files.createDirectories(dataDirectory);
-        }
-        if (!Files.exists(dataFile)){
-            Files.createFile(dataFile);
-        }
+//        if (Files.notExists(dataDirectory)){
+//            Files.createDirectories(dataDirectory);
+//        }
+//        if (!Files.exists(dataFile)){
+//            Files.createFile(dataFile);
+//        }
+//
+//        List<String> fruit = new ArrayList<>();
 
-        List<String> fruit = new ArrayList<>();
+//        fruit.add("banana");
+//        fruit.add("kiwi");
+//        fruit.add("orange");
+//        fruit.add("mango");
 
-        fruit.add("banana");
-        fruit.add("kiwi");
-        fruit.add("orange");
-        fruit.add("mango");
-        Files.write(dataFile,fruit);
+        Files.write(
+                Paths.get("data", "IOtest.txt"),
+                Arrays.asList("strawberry"),
+                StandardOpenOption.APPEND
+        );
 
-        List<String> lines = Files.readAllLines(dataFile);
+//        List<String> lines = Files.readAllLines(dataFile);
 
-        for (String line : lines){
-            System.out.println(line);
-        }
+//        for (String line : lines){
+//            System.out.println(line);
+//        }
     } catch (IOException ioe){
         System.out.println(ioe);
     }
